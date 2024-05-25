9374b16abc29245363715ab5357b966f6f6369c7
veloc1tymeow <trashixx775@gmail.com>
2024-05-25 12:11:36 +0300

    fixxx

--- middlewares/games.js
+++ middlewares/games.js
@@ -57,6 +57,10 @@ const deleteGame = async (req, res, next) => {
   }
 };
 const checkEmptyFields = async (req, res, next) => {
+  if (req.isVoteRequest) {
+    next();
+    return;
+  }
   if (
     !req.body.title ||
     !req.body.description ||
@@ -72,6 +76,10 @@ const checkEmptyFields = async (req, res, next) => {
 };
 
 const checkIfCategoriesAvaliable = async (req, res, next) => {
+  if (req.isVoteRequest) {
+    next();
+    return;
+  }
   if (!req.body.categories || req.body.categories.length === 0) {
     res.setHeader("Content-Type", "application/json");
     res
